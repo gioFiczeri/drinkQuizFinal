c2b66c327fe011759997920ef48428c4a2d4942d
gioFiczeri <97483261+gioFiczeri@users.noreply.github.com>
2022-05-03 23:59:47 -0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,68 @@
+const questions = [
+  {
+    "question": "Age range?",
+    "answer1": "21-30",
+    "answer1Total": "1",
+    "answer2": "31 - 40",
+    "answer2Total": "2",
+    "answer3": "over 40",
+    "answer3Total": "3"
+  },
+  {
+    "question": "I wish I was more social",
+    "answer1": "Agree",
+    "answer1Total": "1",
+    "answer2": "Neutral",
+    "answer2Total": "2",
+    "answer3": "Disagree",
+    "answer3Total": "3"
+  },
+  {
+    "question":
+      "Select what would make you happiest right now",
+    "answer1": "Love",
+    "answer1Total": "1",
+    "answer2": "Money",
+    "answer2Total": "3",
+    "answer3": "Family",
+    "answer3Total": "2"
+  },
+  {
+    "question": "What shape describes you best?",
+    "answer1": "Square",
+    "answer1Total": "3",
+    "answer2": "Circle",
+    "answer2Total": "2",
+    "answer3":"Triangle",
+    "answer3Total": "1"
+  },
+  {
+    "question": "If you could only eat your favorite food for the rest of your life, would you?",
+    "answer1": "Agree",
+    "answer1Total": "1",
+    "answer2": "Neutral",
+    "answer2Total": "2",
+    "answer3": "Disagree",
+    "answer3Total": "3"
+  },
+  {
+    "question":
+      "You make plans with a friend and they cancel on you, what do you do?",
+    "answer1":
+      "Say \"whatever\" and plan a night that'll be GREAT so they don't cancel again.",
+    "answer1Total": "3",
+    "answer2": "Feel hurt because you were looking forward to tonight.",
+    "answer2Total": "2",
+    "answer3": "No problem, you kinda wanted to stay home anyway.",
+    "answer3Total": "1"
+  },
+  {
+    "question": "Which of the following colours do you like most?",
+    "answer1": "Black",
+    "answer1Total": "1",
+    "answer2": "Yellow or light blue",
+    "answer2Total": "2",
+    "answer3": "Red or orange",
+    "answer3Total": "3"
+  }
+]
